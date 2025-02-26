63acea4fcf67e4c22cb85dc951278b8600d213b8
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2025-02-26 23:24:17 +0200

    dd

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
     return (
         <Routes>
+            <Route path="/middlewareredux" element={<MiddleWareTest />} />
             <Route path="/" element={<MiddleWareTest />} />
             <Route path="/post/:postId" element={<PostPage />} />
         </Routes>
